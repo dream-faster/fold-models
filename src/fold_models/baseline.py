b3d56b35772ce15d973d5539cb18d03052822abe
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-05-01 22:24:46 +0200

    feature(Baselines): added ExponentiallyWeightedMovingAverage

--- src/fold_models/baseline.py
+++ src/fold_models/baseline.py
@@ -61,12 +61,48 @@ class MovingAverage(TimeSeriesModel):
         self, X: pd.DataFrame, past_y: pd.Series
     ) -> Union[pd.Series, pd.DataFrame]:
         # it's an online transformation, so len(X) will be always 1,
-        return pd.Series(past_y[-self.window_size :].mean(), index=X.index[-1:None])
+        return pd.Series(
+            past_y.rolling(self.window_size).mean()[-1], index=X.index[-1:None]
+        )
 
     def predict_in_sample(
         self, X: pd.DataFrame, past_y: pd.Series
     ) -> Union[pd.Series, pd.DataFrame]:
         return past_y.rolling(self.window_size).mean()
+
+    fit = fit_noop
+    update = fit
+
+
+class ExponentiallyWeightedMovingAverage(TimeSeriesModel):
+    """
+    A model that predicts the exponentially weighed mean of the last values seen.
+    """
+
+    name = "ExponentiallyWeightedMovingAverage"
+
+    def __init__(self, window_size: int) -> None:
+        self.window_size = window_size
+        self.properties = TimeSeriesModel.Properties(
+            requires_X=False,
+            mode=TimeSeriesModel.Properties.Mode.online,
+            memory_size=window_size,
+            _internal_supports_minibatch_backtesting=True,
+        )
+
+    def predict(
+        self, X: pd.DataFrame, past_y: pd.Series
+    ) -> Union[pd.Series, pd.DataFrame]:
+        # it's an online transformation, so len(X) will be always 1,
+        return pd.Series(
+            past_y.ewm(self.window_size).mean()[-1],
+            index=X.index[-1:None],
+        )
+
+    def predict_in_sample(
+        self, X: pd.DataFrame, past_y: pd.Series
+    ) -> Union[pd.Series, pd.DataFrame]:
+        return past_y.ewm(self.window_size).mean()
 
     fit = fit_noop
     update = fit
