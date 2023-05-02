5cd0337ea763193260e71725413cf8e1ebed5b5f
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-05-02 12:14:25 +0200

    feature(Baselines): added ExponentiallyWeightedMovingAverage (#19)

--- tests/test_baselines.py
+++ tests/test_baselines.py
@@ -1,10 +1,15 @@
 import numpy as np
+import pytest
 from fold.loop import train_backtest
 from fold.splitters import ExpandingWindowSplitter
 from fold.transformations.dev import Test
 from fold.utils.tests import generate_sine_wave_data
 
-from fold_models.baseline import MovingAverage, NaiveSeasonal
+from fold_models.baseline import (
+    ExponentiallyWeightedMovingAverage,
+    MovingAverage,
+    NaiveSeasonal,
+)
 
 
 def check_if_not_nan(x):
@@ -14,34 +19,16 @@ def check_if_not_nan(x):
 test_assert = Test(fit_func=check_if_not_nan, transform_func=lambda X: X)
 
 
-def test_baseline_naive_seasonal() -> None:
-    X, y = generate_sine_wave_data(
-        cycles=10, length=120, freq="M"
-    )  # create a sine wave with yearly seasonality
-
-    splitter = ExpandingWindowSplitter(initial_train_window=0.2, step=0.1)
-    transformations = [
-        NaiveSeasonal(seasonal_length=12),
-        test_assert,
-    ]
-    pred, _ = train_backtest(transformations, X, y, splitter)
-    assert np.isclose(
-        pred.squeeze(), y[pred.index], atol=0.02
-    ).all()  # last year's value should match this year's value, with the sine wave we generated
-    assert (
-        len(pred) == 120 * 0.8
-    )  # should return non-NaN predictions for the all out-of-sample sets
-
-
-def test_baseline_naive_seasonal_online() -> None:
+@pytest.mark.parametrize("online", [True, False])
+def test_baseline_naive_seasonal(online: bool) -> None:
     X, y = generate_sine_wave_data(
         cycles=10, length=120, freq="M"
     )  # create a sine wave with yearly seasonality
 
     splitter = ExpandingWindowSplitter(initial_train_window=0.2, step=0.1)
     naive_seasonal = NaiveSeasonal(seasonal_length=12)
-    naive_seasonal.properties._internal_supports_minibatch_backtesting = False
-    pred, _ = train_backtest(naive_seasonal, X, y, splitter)
+    naive_seasonal.properties._internal_supports_minibatch_backtesting = not online
+    pred, _ = train_backtest([naive_seasonal, test_assert], X, y, splitter)
     assert np.isclose(
         pred.squeeze(), y[pred.index], atol=0.02
     ).all()  # last year's value should match this year's value, with the sine wave we generated
@@ -50,14 +37,13 @@ def test_baseline_naive_seasonal_online() -> None:
     )  # should return non-NaN predictions for the all out-of-sample sets
 
 
-def test_baseline_mean() -> None:
+@pytest.mark.parametrize("online", [True, False])
+def test_baseline_mean(online: bool) -> None:
     X, y = generate_sine_wave_data(cycles=10, length=400)
     splitter = ExpandingWindowSplitter(initial_train_window=0.2, step=0.1)
-    transformations = [
-        MovingAverage(window_size=12),
-        test_assert,
-    ]
-    pred, _ = train_backtest(transformations, X, y, splitter)
+    ma = MovingAverage(window_size=12)
+    ma.properties._internal_supports_minibatch_backtesting = not online
+    pred, _ = train_backtest([ma, test_assert], X, y, splitter)
     assert np.isclose(
         y.shift(1).rolling(12).mean()[pred.index], pred.squeeze(), atol=0.01
     ).all()
@@ -66,14 +52,17 @@ def test_baseline_mean() -> None:
     )  # should return non-NaN predictions for the all out-of-sample sets
 
 
-def test_baseline_mean_online() -> None:
+@pytest.mark.parametrize("online", [True, False])
+def test_baseline_ewmean(online: bool) -> None:
     X, y = generate_sine_wave_data(cycles=10, length=400)
     splitter = ExpandingWindowSplitter(initial_train_window=0.2, step=0.1)
-    ma = MovingAverage(window_size=12)
-    ma.properties._internal_supports_minibatch_backtesting = False
-    pred, _ = train_backtest(ma, X, y, splitter)
+    ma = ExponentiallyWeightedMovingAverage(window_size=12)
+    ma.properties._internal_supports_minibatch_backtesting = not online
+    pred, _ = train_backtest([ma, test_assert], X, y, splitter)
     assert np.isclose(
-        y.shift(1).rolling(12).mean()[pred.index], pred.squeeze(), atol=0.01
+        y.shift(1).ewm(alpha=1 / 12, adjust=True).mean()[pred.index],
+        pred.squeeze(),
+        atol=0.01,
     ).all()
     assert (
         len(pred) == 400 * 0.8
